0beac16c61149eec990d0b99b2ae8d8524a0b76b
Evan Finkelstein <evanfinkelstein91@gmail.com>
2021-02-17 14:15:12 -0800

    wrote tests

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostList from '../display/PostList'
+import Form from '../display/Form'
+export default function App() {
+  return (
+    <>
+      <Form />
+      <PostList />
+    </>
+  )
+    ;
+}
---
+++ src/selectors/postSelectores.test.js
@@ -0,0 +1,13 @@
+import { getPosts } from './postSelectors';
+
+describe('post selectors', () => {
+    it('select the list of posts from state', () => {
+        const state = {
+            posts: [{ title: 'title', body: 'body' }]
+        };
+
+        const posts = getPosts(state);
+
+        expect(posts).toEqual([{ title: 'title', body: 'body' }]);
+    });
+});
